3da19a5edaf5ed332103ee42fdbd3c611b855909
Hashir <mail2hashirpm@gmail.com>
2025-02-09 20:43:11 +0530

    refactor: rename frontend and agent package names for consistency

--- index.ts
+++ index.ts
@@ -10,40 +10,29 @@ import {
   scanForRaceOpportunities,
   validateEnvironment,
 } from "./src/agent-kit/robo-agent";
-import {
-  generateRaceCreationTweet,
-  generateRaceEndTweet,
-  generateRaceProgressTweet,
-  generateRaceStartTweet,
-  generateStakingEndTweet,
-  generateStakingStartTweet,
-} from "./src/tweet/race-tweets";
-import { createTweetAPI, getProfileOfAnAccount } from "./src/scrapers/twitter";
-
 dotenv.config();
 
 const INTERVAL = 5000; // 5 seconds in milliseconds
 
 async function main() {
   console.log("Starting Agent...");
-    validateEnvironment();
-    const { agent, config } = await initializeAgent();
-    await registerWalletBasename();
-    setInterval(async () => {
-      await scanForRaceOpportunities();
-    }, INTERVAL);
-    setInterval(async () => {
-      await fetchNewTraps();
-    }, INTERVAL);
-    setInterval(async () => {
-      await fetchNewStakes();
-    }, INTERVAL);
-    setInterval(async () => {
-      await fetchNewRaces();
-    }, INTERVAL);
+  validateEnvironment();
+  await initializeAgent();
+  await registerWalletBasename();
+  setInterval(async () => {
+    await scanForRaceOpportunities();
+  }, INTERVAL);
+  setInterval(async () => {
+    await fetchNewTraps();
+  }, INTERVAL);
+  setInterval(async () => {
+    await fetchNewStakes();
+  }, INTERVAL);
+  setInterval(async () => {
+    await fetchNewRaces();
+  }, INTERVAL);
 
-
-    console.log("Scheduled tasks are running every 5 seconds.");
+  console.log("Scheduled tasks are running every 5 seconds.");
 }
 
 // Run the main function
